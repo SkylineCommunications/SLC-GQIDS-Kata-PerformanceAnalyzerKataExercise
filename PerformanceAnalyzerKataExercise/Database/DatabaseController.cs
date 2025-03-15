6561801c4221f58f3448a672bf6ad811ec165e71
Edib Šupić <edib.supic@skyline.be>
2025-03-15 01:13:30 +0100

    Refactored everything.

--- PerformanceAnalyzerKataExercise/Database/DatabaseController.cs
+++ PerformanceAnalyzerKataExercise/Database/DatabaseController.cs
@@ -1,112 +1,78 @@
 ﻿namespace PerformanceAnalyzerKataExercise.Database
 {
-	using System;
 	using System.Collections.Generic;
-	using System.Linq;
 
 	using PerformanceAnalyzerKataExercise.Database.Models;
 
 	internal class DatabaseController
 	{
-		internal void Connect()
+		internal List<Employee> GetAllEmployees()
 		{
+			Connect();
+			var allEmployees = Fetch();
+			var processedEmployees = Process(allEmployees);
+			Store(processedEmployees);
+			Disconnect();
+
+			return allEmployees;
+		}
+
+		private void Connect()
+		{
+			Authenticate();
+			Authorize();
+
 			MockExecution.Connect();
 		}
 
-		internal List<Employee> ExecuteQuery(Query query, string filter)
+		private void Authenticate()
 		{
-			if (String.IsNullOrWhiteSpace(filter))
-			{
-				return GetEmployees();
-			}
-
-			switch (query)
-			{
-				case Query.GetEmployees:
-					return GetEmployees();
-
-				case Query.GetEmployeeById:
-					return GetEmployeeById(filter);
-
-				case Query.GetEmployeesByName:
-					return GetEmployeesByName(filter);
-
-				case Query.GetEmployeesByRole:
-					return GetEmployeesByRole(filter);
-
-				case Query.GetEmployeesByDepartment:
-					return GetEmployeesByDepartment(filter);
-
-				case Query.GetEmployeesByLocation:
-					return GetEmployeesByLocation(filter);
-
-				default:
-					return new List<Employee>();
-			}
-		}
-
-		internal void Disconnect()
-		{
-			MockExecution.Disconnect();
-		}
-
-		private List<Employee> GetEmployees()
-		{
-			Authorize();
-
-			MockExecution.GetEmployees();
-
-			return MockDatabase.Table;
-		}
-
-		private List<Employee> GetEmployeeById(string filter)
-		{
-			Authorize();
-
-			MockExecution.GetEmployeeById();
-
-			return MockDatabase.Table.Where(employee => employee.Equals(Query.GetEmployeeById, filter)).ToList();
-		}
-
-		private List<Employee> GetEmployeesByName(string filter)
-		{
-			Authorize();
-
-			MockExecution.GetEmployeesByName();
-
-			return MockDatabase.Table.Where(employee => employee.Equals(Query.GetEmployeesByName, filter)).ToList();
-		}
-
-		private List<Employee> GetEmployeesByRole(string filter)
-		{
-			Authorize();
-
-			MockExecution.GetEmployeesByRole();
-
-			return MockDatabase.Table.Where(employee => employee.Equals(Query.GetEmployeesByRole, filter)).ToList();
-		}
-
-		private List<Employee> GetEmployeesByDepartment(string filter)
-		{
-			Authorize();
-
-			MockExecution.GetEmployeesByDepartment();
-
-			return MockDatabase.Table.Where(employee => employee.Equals(Query.GetEmployeesByDepartment, filter)).ToList();
-		}
-
-		private List<Employee> GetEmployeesByLocation(string filter)
-		{
-			Authorize();
-
-			MockExecution.GetEmployeesByLocation();
-
-			return MockDatabase.Table.Where(employee => employee.Equals(Query.GetEmployeesByLocation, filter)).ToList();
+			MockExecution.Authenticate();
 		}
 
 		private void Authorize()
 		{
 			MockExecution.Authorize();
+		}
+
+		private List<Employee> Fetch()
+		{
+			string query = CreateQuery();
+
+			var allEmployees = MockExecution.Fetch(query);
+
+			return allEmployees;
+		}
+
+		private string CreateQuery()
+		{
+			string query = MockExecution.CreateQuery();
+
+			return query;
+		}
+
+		private List<Employee> Process(List<Employee> employees)
+		{
+			var processedEmployees = new List<Employee>();
+
+			foreach (var employee in employees)
+			{
+				var processedEmployee = MockExecution.Process(employee);
+
+				processedEmployees.Add(processedEmployee);
+			}
+
+			return processedEmployees;
+		}
+
+		private void Store(List<Employee> employees)
+		{
+			MockExecution.Store(employees);
+		}
+
+		private void Disconnect()
+		{
+			MockExecution.Disconnect();
 		}
 	}
 }
--- PerformanceAnalyzerKataExercise/Database/Models/Employee.cs
+++ PerformanceAnalyzerKataExercise/Database/Models/Employee.cs
@@ -1,7 +1,5 @@
 ﻿namespace PerformanceAnalyzerKataExercise.Database.Models
 {
-	using System;
-
 	internal class Employee
 	{
 		internal string Id { get; set; }
@@ -13,37 +11,5 @@
 		internal string Department { get; set; }
 
 		internal string Location { get; set; }
-
-		internal bool Equals(Query query, string filter)
-		{
-			if (String.IsNullOrWhiteSpace(filter))
-			{
-				return true;
-			}
-
-			switch (query)
-			{
-				case Query.GetEmployees:
-					return true;
-
-				case Query.GetEmployeeById:
-					return Id.IndexOf(filter, StringComparison.OrdinalIgnoreCase) != -1;
-
-				case Query.GetEmployeesByName:
-					return Name.IndexOf(filter, StringComparison.OrdinalIgnoreCase) != -1;
-
-				case Query.GetEmployeesByRole:
-					return Role.IndexOf(filter, StringComparison.OrdinalIgnoreCase) != -1;
-
-				case Query.GetEmployeesByDepartment:
-					return Department.IndexOf(filter, StringComparison.OrdinalIgnoreCase) != -1;
-
-				case Query.GetEmployeesByLocation:
-					return Location.IndexOf(filter, StringComparison.OrdinalIgnoreCase) != -1;
-
-				default:
-					return false;
-			}
-		}
 	}
 }
